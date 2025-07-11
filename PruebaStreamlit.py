23c39ceb4b2ce6184f52556e9260727ad29bd2a6
JIM1425 <jimee2030@gmail.com>
2025-07-11 10:21:31 -0600

    update

--- PruebaStreamlit.py
+++ PruebaStreamlit.py
@@ -2,10 +2,12 @@ import streamlit as st
 import pandas as pd
 import joblib
 
+
+
 st.set_page_config(page_title = 'MLpruebaWebApp', layout = 'wide')
 
 pipeline_path = 'artefacts/preprocessor/preprocessor.pkl'
-model_path = 'artefacts/model/svc.pkl'
+model_path = 'artefacts/model/svm.pkl'
 encoder_path = 'artefacts/preprocessor/encoder.pkl'
 
 with open(pipeline_path, 'rb') as file1:
@@ -62,4 +64,60 @@ with col3:
     sc_w = st.slider('Ancho de la pantalla (cm)', min_value = 0, max_value = 18)
     talk_time = st.slider('Duracion de la bateria bajo uso constante (Hrs)', min_value = 2, max_value = 20)
 
-    
+st.divider() #Agrega una linea divisoria
+col4, col5, col6 = st.columns(3)
+
+with col4:
+    blue = st.selectbox('Tiene Bluetooth?', options=[0,1])
+    three_g = st.selectbox('Tiene 3G?', options=[0,1])
+
+with col5:
+    dual_sim = st.selectbox('Tiene Dual SIM?', options= [0,1])
+    touch_screen = st.selectbox('Tiene pantalla tactil?', options=[0,1])
+
+with col6:
+    wifi = st.selectbox('Tiene Wifi?', options= [0,1])
+    four_g = st.selectbox('4G', options= [0,1])
+
+st.divider()  # Agrega una linea divisoria
+
+if st.button('Predecir'):
+    input_data = pd.DataFrame(
+        {
+            'battery_power': [battery_power],
+            'blue': [blue],
+            'clock_speed': [clock_speed],
+            'dual_sim': [dual_sim],
+            'fc': [fc],
+            'four_g': [four_g],
+            'int_memory': [int_memory],
+            'm_dep': [m_dep],
+            'mobile_wt': [mobile_wt],
+            'n_cores': [n_cores],
+            'pc': [pc],
+            'px_height': [px_heigth],
+            'px_width': [px],
+            'ram': [ram],
+            'mobile_wt': [mobile_wt],
+            'sc_h': [sc_h],
+            'sc_w': [sc_w],
+            'talk_time': [talk_time],
+            'three_g' : [three_g],
+            'touch_screen': [touch_screen],
+            'wifi': [wifi],
+            
+       }
+    )
+
+    st.dataframe(input_data)
+    pipeline_data = pipeline.transform(input_data)
+    prediction = model.predict(pipeline_data)
+
+    if prediction[0] == 0:
+        st.success('Bajo costo')
+    elif prediction[0] == 1:
+        st.success('Costo medio')
+    elif prediction[0] == 2:
+        st.success('Alto costo')
+    elif prediction[0] == 3:
+        st.success('Costo muy alto')
